4018b668533d97975628a73b8482eedc95b5b5ba
Pete <petergarvin1@gmail.com>
2019-04-20 20:00:06 +0100

    extensions methods added and code refactored to functional

--- BowlingKata/BowlingKata.js
+++ BowlingKata/BowlingKata.js
@@ -1,19 +1,23 @@
 exports.ScoreCalculator = scoreboard => {
  
-  var missReplacedScoreBoard = scoreboard
-                                .replace(/\-/g, '0')
-                                .replace(/\|{2}/g, '|');
-
-  var splitScoreBoard = missReplacedScoreBoard.split("|");
-
-  if (splitScoreBoard.every(frame => (frame === "00" || frame === ""))) {
-      return 0;
-  }
-
-  var splitScoreBoardInts = splitScoreBoard
-                                    .map(frame => frame
-                                                     .split('')
-                                                     .map(score => +score));
-
-  return splitScoreBoardInts[0].reduce((a,b)=>a+b);
+  return scoreboard.replaceDoublePipeWithSingle()
+                   .replaceHyphensWithZeros()
+                   .split("|")
+                   .charactersToIntsInArray()
+                   [0].reduce((a,b)=>a+b);
 };
+
+String.prototype.replaceDoublePipeWithSingle = function() {
+  return this.replace(/\|{2}/g, '|');
+}
+
+String.prototype.replaceHyphensWithZeros = function() {
+  return this.replace(/\-/g, '0')
+}
+
+Array.prototype.charactersToIntsInArray = function() {
+  return this
+          .map(arrayElement => arrayElement
+                   .split('')
+                   .map(char => +char));
+}
