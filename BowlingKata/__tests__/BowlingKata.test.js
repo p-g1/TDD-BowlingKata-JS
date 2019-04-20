21e1ee5c3ead55b05d987d96b9b735fa59a1a40f
Pete <petergarvin1@gmail.com>
2019-04-20 19:00:32 +0100

    test refactor

--- BowlingKata/__tests__/BowlingKata.test.js
+++ BowlingKata/__tests__/BowlingKata.test.js
@@ -2,16 +2,13 @@ const Bowling = require("../BowlingKata.js");
 require("jest-each");
 
 describe("BowlingKata", () => {
-  it("should return zero when given scoreboard with all misses", () => {
-    expect(Bowling.ScoreCalculator("--|--|--|--|--|--|--|--|--|--||--")).toBe(0);
-  });
-  it("should return correct score when given scoreboard with a single hit", () => {
-    expect(Bowling.ScoreCalculator("1-|--|--|--|--|--|--|--|--|--||--")).toBe(1);
-  });
-  it("should return correct score when given scoreboard with a single hit", () => {
-    expect(Bowling.ScoreCalculator("5-|--|--|--|--|--|--|--|--|--||--")).toBe(5);
-  });
-  it("should return correct score when given scoreboard with a single hit", () => {
-    expect(Bowling.ScoreCalculator("9-|--|--|--|--|--|--|--|--|--||--")).toBe(9);
+  test.each`
+    input   | expectedResult
+    ${"--|--|--|--|--|--|--|--|--|--||--"} | ${0}
+    ${"1-|--|--|--|--|--|--|--|--|--||--"} | ${1}
+    ${"5-|--|--|--|--|--|--|--|--|--||--"} | ${5}
+    ${"9-|--|--|--|--|--|--|--|--|--||--"} | ${9}
+  `("should calculate score of $expectedResult from $input", ({ input, expectedResult }) => {
+    expect(Bowling.ScoreCalculator(input)).toBe(expectedResult);
   });
 });
