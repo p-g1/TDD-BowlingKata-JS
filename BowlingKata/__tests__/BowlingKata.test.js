88f36e4b1de425f9ff1f5d29fd4f003dd0756795
Pete <petergarvin1@gmail.com>
2019-04-20 18:55:43 +0100

    added tests for single hit at pos 1 in first frame

--- BowlingKata/__tests__/BowlingKata.test.js
+++ BowlingKata/__tests__/BowlingKata.test.js
@@ -5,4 +5,13 @@ describe("BowlingKata", () => {
   it("should return zero when given scoreboard with all misses", () => {
     expect(Bowling.ScoreCalculator("--|--|--|--|--|--|--|--|--|--||--")).toBe(0);
   });
+  it("should return correct score when given scoreboard with a single hit", () => {
+    expect(Bowling.ScoreCalculator("1-|--|--|--|--|--|--|--|--|--||--")).toBe(1);
+  });
+  it("should return correct score when given scoreboard with a single hit", () => {
+    expect(Bowling.ScoreCalculator("5-|--|--|--|--|--|--|--|--|--||--")).toBe(5);
+  });
+  it("should return correct score when given scoreboard with a single hit", () => {
+    expect(Bowling.ScoreCalculator("9-|--|--|--|--|--|--|--|--|--||--")).toBe(9);
+  });
 });
