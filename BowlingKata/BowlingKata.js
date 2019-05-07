55930bf729f607f589f81b70931475cdca034535
Pete <petergarvin1@gmail.com>
2019-05-07 22:48:44 +0100

    house on fire

--- BowlingKata/BowlingKata.js
+++ BowlingKata/BowlingKata.js
@@ -2,13 +2,13 @@ exports.ScoreCalculator = scoreboard => {
   return scoreboard
     .replaceDoublePipeWithSingle()
     .replaceHyphensWithZeros()
-    .splitInto2DArray()
-    .handleStrikeCharacters()
-    .handleSpareCharacters()
+    .splitIntoThrowsInFrames2DArray()
+    .handleStrikes()
+    .handleSpares()
     .convertCharsToIntsInArray()
     .handleSpecialCharacterScoring()
-    .sumSubArrays()
-    .add();
+    .sumThrowsInFrame()
+    .sumAllFrames();
 };
 
 String.prototype.replaceDoublePipeWithSingle = function() {
@@ -19,47 +19,75 @@ String.prototype.replaceHyphensWithZeros = function() {
   return this.replace(/\-/g, "0");
 };
 
-String.prototype.splitInto2DArray = function() {
-  return this.split("|").map(arrayElement => arrayElement.split(""));
+String.prototype.splitIntoThrowsInFrames2DArray = function() {
+  return this.split("|").map(frame => frame.split(""));
 }
 
-Array.prototype.handleStrikeCharacters = function() {
-  return this.map(subArray => subArray
-    .map(char => 
-         char == "/" ? char 
-         : char == "X" ? 10 
-         : char));
+Array.prototype.handleStrikes = function() {
+  return this.map(throwsInFrame => throwsInFrame
+    .map(ball => 
+         ball.isSpare() 
+         ? ball 
+         : ball.isStrike()
+         ? 10 
+         : ball
+         ));
 }
 
-Array.prototype.handleSpareCharacters = function() {
-  for (var i = 0; i < this.length; i++) {
-    this[i][1] == "/" ? this[i][1] = 10 - Number(this[i][0]) : this[i][1];
+Array.prototype.handleSpares = function() {
+  let frames = this, frameThrow1, frameThrow2;
+  for (var frameIndex = 0; frameIndex < frames.length; frameIndex++) {
+     frameThrow1 = frames[frameIndex][0];
+     frameThrow2 = frames[frameIndex][1];
+    
+    if (frameThrow2 == "/") {
+      
+      frameThrow2 = (10 - Number(frameThrow1));
+      console.log(frames[frameIndex][1], frameThrow2);
+    }
+    
   } 
-  return this;
+  console.log(frames);
+  return frames;
 }
 
 Array.prototype.convertCharsToIntsInArray = function() {
-    return this.map(subArray => subArray.map(char => Number(char)));
+    return this.map(throwsInFrame => throwsInFrame.map(ball => Number(ball)));
 };
 
 Array.prototype.handleSpecialCharacterScoring = function() {
-  for (var i = 0; i < this.length-2; i++) {
-    if (this[i][0] === 10) {
-        this[i+1][0] === 10 ?
-                  this[i][0] += this[i+1][0] + this[i+2][0]
-                  :this[i][0] += this[i+1][0] + this[i+1][1]; 
+  let frames = this;
+  for (var frameIndex = 0; frameIndex < frames.length-2; frameIndex++) {
+    let frameThrow1 = frames[frameIndex][0];
+    let frameThrow2 = frames[frameIndex][1];
+    let nextFrameThrow1 = frames[frameIndex+1][0];
+    let nextFrameThrow2 = frames[frameIndex+1][1];
+    let nextFramePlusOneThrow1 = frames[frameIndex+2][0];
+
+    if (frameThrow1 === 10) {
+      nextFrameThrow1 === 10 
+      ? frames[frameIndex][0] += frames[frameIndex+1][0] + frames[frameIndex+2][0]
+      : frames[frameIndex][0] += frames[frameIndex+1][0] + frames[frameIndex+1][1]; 
     }
-    else if (this[i].reduce((a,b)=>a+b) === 10) {
-      this[i][1] += this[i+1][0];
+    else if (frames[frameIndex].reduce((a,b)=>a+b) === 10) {
+      frameThrow2 += nextFrameThrow1;
     }
   }
   return this;
 }
 
-Array.prototype.sumSubArrays = function() {
-  return this.map(subArray => subArray.reduce((a, b) => a + b));
+Array.prototype.sumThrowsInFrame = function() {
+  return this.map(throwsInFrame => throwsInFrame.reduce((a, b) => a + b));
 }
 
-Array.prototype.add = function() {
+Array.prototype.sumAllFrames = function() {
   return this.reduce((a, b) => a + b);
+}
+
+String.prototype.isSpare = function() {
+  return this == "/";
+}
+
+String.prototype.isStrike = function() {
+  return this == "X";
 }
